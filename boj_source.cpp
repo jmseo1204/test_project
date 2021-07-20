eb8bf995a8a8bb49d4c2ba0142078e416f097b00
Jungmin Seo <jmseo1204@snu.ac.kr>
2021-07-20 21:42:07 +0900

    11726

---
+++ boj_source.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+//11726
+
+int pibo(int n, int h, int t){
+	if (n==0){
+		return t;
+	}else{
+		return pibo(n-1, t, (h+t)%10007);
+	}
+}
+
+int main(){
+	int a;
+	cin >> a;
+	cout << pibo(a, 0, 1);
+
+}
